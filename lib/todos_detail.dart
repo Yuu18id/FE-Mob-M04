7384d5f4e9071c8d3bb80cdd96222a393a815cea
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2023-03-27 16:22:49 +0700

    Add files via upload

---
+++ lib/todos_detail.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class TodosDetailPage extends StatelessWidget {
+  final String kegiatan;
+  final String keterangan;
+  final String tglMulai;
+  final String tglSelesai;
+  const TodosDetailPage({super.key, required this.kegiatan, required this.keterangan, required this.tglMulai, required this.tglSelesai});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: [
+          Row(
+            children: [
+              Icon(Icons.circle),
+              Column(
+                children: [
+                  Text(kegiatan.toString()),
+                  Text(keterangan.toString()),
+                ],
+              ),
+              Column(
+                  children: [
+                    Text(tglMulai),
+          Text(tglSelesai)
+                  ],
+              )
+            ],
+          ),
+          
+          
+          
+        ]),
+      ),
+    );
+  }
+}
